e3066a7e6668e49a8c404fc9d4c61d85bc58c15a
Yongkyun Shin <yongkyuns@gmail.com>
2020-01-06 13:13:20 +0900

    changed doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
@@ -83,7 +83,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
